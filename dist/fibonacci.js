059a356697830fc613b097076f2be1daad0e58f0
Gozman <thegzmn@gmail.com>
2023-02-22 21:21:16 +0100

    Update

---
+++ dist/fibonacci.js
@@ -0,0 +1,34 @@
+"use strict";
+//! Creating the Fibonacci Sequence using iterative and recyrsive methods.
+//! Using iteration
+const fibs = (number) => {
+    const fibsArray = [];
+    let initial = 0;
+    let current = 1;
+    for (let i = 0; i < number; i++) {
+        fibsArray.push(initial);
+        let temp = current;
+        current = initial + current;
+        initial = temp;
+    }
+    return fibsArray;
+};
+//! Using recursion
+const fibsRec = (n) => {
+    if (n <= 0) {
+        return [];
+    }
+    else if (n === 1) {
+        return [0];
+    }
+    else if (n === 2) {
+        return [0, 1];
+    }
+    else {
+        const fibsArray = fibsRec(n - 1);
+        fibsArray.push(fibsArray[fibsArray.length - 1] + fibsArray[fibsArray.length - 2]);
+        return fibsArray;
+    }
+};
+console.log(fibs(12));
+console.log(fibsRec(12));
---
+++ src/fibonacci.ts
@@ -0,0 +1,36 @@
+//! Creating the Fibonacci Sequence using iterative and recyrsive methods.
+
+//! Using iteration
+const fibs = (number: number): number[] => {
+  const fibsArray: number[] = [];
+
+  let initial = 0;
+  let current = 1;
+
+  for (let i = 0; i < number; i++) {
+    fibsArray.push(initial);
+    let temp = current;
+    current = initial + current;
+    initial = temp;
+  }
+
+  return fibsArray;
+};
+
+//! Using recursion
+const fibsRec = (n: number): number[] => {
+  if (n <= 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1]
+  } else {
+    const fibsArray = fibsRec(n - 1);
+    fibsArray.push(fibsArray[fibsArray.length - 1] + fibsArray[fibsArray.length - 2]);
+    return fibsArray;
+  }
+};
+
+console.log(fibs(12));
+console.log(fibsRec(12));
